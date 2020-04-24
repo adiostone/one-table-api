e4bcec122008184a914f67850680bdc0a03de6c9
ihooni <to@ihooni.com>
2020-04-24 20:38:54 +0900

    chore: create user table when boot db service

--- src/providers/DBServiceProvider.ts
+++ src/providers/DBServiceProvider.ts
@@ -1,7 +1,18 @@
 import MySQLConnector from '@/modules/database/MySQLConnector'
+import User, { userSchema } from '@/models/User'
 
 export default class DBServiceProvider {
   public static async boot(): Promise<void> {
+    // connect to database
     await MySQLConnector.I.connect()
+
+    /* create tables */
+
+    User.init(userSchema, {
+      tableName: 'users',
+      sequelize: MySQLConnector.I.conn
+    })
+
+    await User.sync()
   }
 }
