6ebb9574ce4e4567d2142f94c9b99ba83c27ebb9
ihooni <to@ihooni.com>
2020-04-26 02:44:02 +0900

    chore: add handshaking controller for table user

--- src/http/controller/TableAuthController.ts
+++ src/http/controller/TableAuthController.ts
@@ -1,24 +1,33 @@
 import { SimpleHandler } from '@/http/HttpHandler'
 import User from '@/models/User'
+import JWTIssuer from '@/modules/internal/JWTIssuer'
 import url from 'url'
 
 export default class TableAuthController {
   public static handshake: SimpleHandler = async (req, res) => {
     const user = req.user as User
+
+    // issue tokens
+    const accessToken = JWTIssuer.issueTableAccessToken(user.id)
+    const refreshToken = JWTIssuer.issueTableRefreshToken()
+
     let handshakingItems
-
-    // TODO: create tokens
-
     if (user.isNewRecord) {
       await user.save()
 
       handshakingItems = {
-        isnew: true
+        access: accessToken,
+        refresh: refreshToken,
+        isnew: true // query for notifying that this user is new face
       }
     } else {
-      handshakingItems = {}
+      handshakingItems = {
+        access: accessToken,
+        refresh: refreshToken
+      }
     }
 
+    // redirect to handshaking place
     res.redirect(
       url.format({
         pathname: process.env.TB_HANDSHAKE_URL,
