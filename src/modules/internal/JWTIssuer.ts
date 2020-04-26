910dfab735bf04c0b44204f26e32b9d824cd338a
ihooni <to@ihooni.com>
2020-04-27 03:53:09 +0900

    chore: check if refresh token is soon expired

--- src/modules/internal/JWTIssuer.ts
+++ src/modules/internal/JWTIssuer.ts
@@ -1,10 +1,18 @@
 import jwt from 'jsonwebtoken'
+import ms from 'ms'
 
 const TABLE_CONFIGS = {
   secret: process.env.TB_JWT_SECRET_KEY,
   issuer: process.env.TB_JWT_ISSUER,
   accessExpire: '1h',
-  refreshExpire: '30d'
+  refreshExpire: '30d',
+  maxAgeToRefresh: '7d'
+}
+
+interface DecodedRefreshToken {
+  payload: {
+    exp: number
+  }
 }
 
 export default class JWTIssuer {
@@ -23,5 +31,14 @@ export default class JWTIssuer {
       expiresIn: TABLE_CONFIGS.refreshExpire,
       notBefore: 1
     })
+  }
+
+  public static isRefreshTokenSoonExpired(token: string): boolean {
+    const decoded = jwt.decode(token, { complete: true }) as DecodedRefreshToken
+    const refreshThreshold = new Date(
+      decoded.payload.exp * 1000 - ms(TABLE_CONFIGS.maxAgeToRefresh)
+    )
+
+    return Date.now() > refreshThreshold.getTime()
   }
 }
