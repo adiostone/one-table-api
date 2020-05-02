5cf4b35817c0f8646feec63eff4448ea66b837d2
ihooni <to@ihooni.com>
2020-05-02 22:35:18 +0900

    chore: temp router for test

--- src/routes/v1/table/index.ts
+++ src/routes/v1/table/index.ts
@@ -1,18 +1,20 @@
 import express from 'express'
 import authRouter from '@/routes/v1/table/auth'
+import passport from 'passport'
+import CheckTokenBlacklist from '@/http/middleware/CheckTokenBlacklist'
 
 const tableRouter = express.Router()
 
 tableRouter.use('/auth', authRouter)
 
-// // example router
-// tableRouter.get(
-//   '/test',
-//   passport.authenticate('jwt-access-table', { session: false }),
-//   CheckTokenBlacklist.handler,
-//   (req, res) => {
-//     res.send('good!')
-//   }
-// )
+// temp router for test
+tableRouter.get(
+  '/test',
+  passport.authenticate('jwt-access-table', { session: false }),
+  CheckTokenBlacklist.handler,
+  (req, res) => {
+    res.send('good!')
+  }
+)
 
 export default tableRouter
