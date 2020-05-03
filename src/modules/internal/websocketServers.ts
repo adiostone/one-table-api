8c87bf3436026d56cd22aa7812b3188b36251b0a
ihooni <to@ihooni.com>
2020-05-03 21:57:37 +0900

    chore: init party websocket server

---
+++ src/modules/internal/websocketServers.ts
@@ -0,0 +1,13 @@
+import WebSocket from 'ws'
+
+const partyWS = new WebSocket.Server({ noServer: true })
+partyWS.on('connection', ws => {
+  ws.send('hello table!!')
+
+  ws.on('message', msg => {
+    console.log(msg)
+    ws.send(`you said, ${msg}`)
+  })
+})
+
+export { partyWS }
