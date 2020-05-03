112ac30d13aba0d8f10d7a809addf778c32a212a
ihooni <to@ihooni.com>
2020-05-03 21:58:01 +0900

    chore: add controller for party

---
+++ src/http/controller/TablePartyController.ts
@@ -0,0 +1,10 @@
+import { SimpleHandler } from '@/http/HttpHandler'
+import { partyWS } from '@/modules/internal/websocketServers'
+
+export default class TablePartyController {
+  public static upgradeToWebSocket: SimpleHandler = (req, res) => {
+    partyWS.handleUpgrade(req, req.socket, Buffer.from(''), ws => {
+      partyWS.emit('connection', ws, req)
+    })
+  }
+}
--- src/routes/v1/table/index.ts
+++ src/routes/v1/table/index.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import authRouter from '@/routes/v1/table/auth'
 import passport from 'passport'
 import CheckTokenBlacklist from '@/http/middleware/CheckTokenBlacklist'
+import TablePartyController from '@/http/controller/TablePartyController'
+import HttpErrorHandler from '@/http/middleware/HttpErrorHandler'
 
 const tableRouter = express.Router()
 
@@ -16,5 +18,15 @@ tableRouter.get(
     res.send('good!')
   }
 )
+
+/**
+ * GET: Start party websocket connection
+ *
+ * TODO: Apply JWT authentication check
+ */
+tableRouter
+  .route('/party')
+  .get(TablePartyController.upgradeToWebSocket)
+  .all(HttpErrorHandler.methodNotAllowedHandler)
 
 export default tableRouter
