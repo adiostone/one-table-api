38c9e0d1d50ff8965d5cc0ce54eacc365e5927bb
ihooni <to@ihooni.com>
2020-05-08 23:11:08 +0900

    chore: use combined middleware

--- src/routes/v1/table/index.ts
+++ src/routes/v1/table/index.ts
@@ -2,8 +2,6 @@ import express from 'express'
 import authRouter from '@/routes/v1/table/auth'
 import meRouter from '@/routes/v1/table/me'
 import { CheckUserSignIn } from '@/http/middleware/combinedMiddleware'
-import passport from 'passport'
-import CheckTokenBlacklist from '@/http/middleware/CheckTokenBlacklist'
 import TablePartyController from '@/http/controller/TablePartyController'
 import HttpErrorHandler from '@/http/middleware/HttpErrorHandler'
 
@@ -12,17 +10,12 @@ const tableRouter = express.Router()
 tableRouter.use('/auth', authRouter)
 tableRouter.use('/me', CheckUserSignIn, meRouter)
 
-
 /**
  * GET: Start party websocket connection
  */
 tableRouter
   .route('/party')
-  .get(
-    passport.authenticate('jwt-access-table', { session: false }),
-    CheckTokenBlacklist.handler,
-    TablePartyController.upgradeToWebSocket
-  )
+  .get(CheckUserSignIn, TablePartyController.upgradeToWebSocket)
   .all(HttpErrorHandler.methodNotAllowedHandler)
 
 export default tableRouter
