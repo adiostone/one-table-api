587798455d8879064cffc01d4e2a40f3b607c441
ihooni <to@ihooni.com>
2020-05-11 04:09:42 +0900

    chore: add new events - create party, notify new party

--- src/http/websocket/partyServer.ts
+++ src/http/websocket/partyServer.ts
@@ -2,15 +2,24 @@ import WebSocket from 'ws'
 import User from '@/models/User'
 import { HttpRequest } from '@/http/HttpHandler'
 import ms from 'ms'
+import PartyManager, { PartyRoom } from '@/modules/internal/PartyManager'
 
-interface PartyWS extends WebSocket {
+export interface PartyWS extends WebSocket {
   isAlive: boolean
   user: User
+  joinedParty: string
 }
 
 interface PartyMessage {
   operation: string
   body?: {}
+}
+
+interface CreatePartyBody {
+  restaurantID: number
+  title: string
+  address: string
+  capacity: number
 }
 
 // create websocket server
@@ -26,20 +35,34 @@ const aliveCheckingSchedule = setInterval(() => {
 
     // send ping for alive checking
     ws.isAlive = false
-    ws.ping()
+    ws.emit('semiPing')
+    // ws.ping()
   })
 }, ms('30s'))
 
 partyServer.on('connection', (ws: PartyWS, req: HttpRequest) => {
   ws.isAlive = true
   ws.user = req.user as User
+  ws.joinedParty = null
+
+  ws.on('semiPing', () => {
+    const message: PartyMessage = {
+      operation: 'ping'
+    }
+
+    ws.send(JSON.stringify(message))
+  })
 
   ws.on('pong', () => {
     ws.isAlive = true
   })
 
+  // ws.on('close', (code, reason) => {})
+
   ws.on('message', msg => {
     const message: PartyMessage = JSON.parse(msg as string)
+
+    console.log(JSON.stringify(message))
 
     // check if registered operation
     if (ws.listeners(message.operation).length > 0) {
@@ -47,6 +70,31 @@ partyServer.on('connection', (ws: PartyWS, req: HttpRequest) => {
     } else {
       ws.send('ERROR: wrong type operation')
     }
+  })
+
+  ws.on('createParty', (body: CreatePartyBody) => {
+    PartyManager.I.createParty(
+      ws,
+      body.restaurantID,
+      body.title,
+      body.address,
+      body.capacity
+    ).then()
+  })
+
+  ws.on('notifyNewParty', (newPartyID: string, newParty: PartyRoom) => {
+    const message: PartyMessage = {
+      operation: 'notifyNewParty',
+      body: {
+        id: newPartyID,
+        restaurantName: newParty.restaurant.get('name'),
+        title: newParty.title,
+        address: newParty.address,
+        capacity: newParty.capacity
+      }
+    }
+
+    ws.send(JSON.stringify(message))
   })
 })
 
