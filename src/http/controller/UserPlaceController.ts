b8c0359111bc6408a5a83749f3dd6398d7e554c7
ihooni <to@ihooni.com>
2020-05-10 16:02:48 +0900

    chore: response with empty data when data is not exist

--- src/http/controller/UserPlaceController.ts
+++ src/http/controller/UserPlaceController.ts
@@ -22,11 +22,7 @@ export default class UserPlaceController {
     const userPlace = await UserPlace.findByPk(user.get('id'))
 
     if (userPlace === null) {
-      res.status(404).json({
-        err: {
-          msg: "This user's place not exist"
-        }
-      })
+      res.status(200).json()
     } else {
       const body: GetResponseBody = {
         latitude: userPlace.get('latitude'),
