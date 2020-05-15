e19232444ae3b5ad14d6f7481a6d78cf9e228cb4
ihooni <to@ihooni.com>
2020-05-15 23:41:50 +0900

    refactor: party server

---
+++ src/modules/internal/party/partyServer.ts
@@ -0,0 +1,85 @@
+import WebSocket from 'ws'
+import User from '@/models/User'
+import { HttpRequest } from '@/http/HttpHandler'
+import { setInterval } from 'timers'
+import ms from 'ms'
+
+export interface PartyWS extends WebSocket {
+  isAlive: boolean
+  user: User
+}
+
+interface PartyMessage {
+  operation: string
+  body?: {}
+}
+
+const partyServer = new WebSocket.Server({ noServer: true })
+
+// repeatedly check heartbeat of each clients
+const heartbeat = setInterval(() => {
+  partyServer.clients.forEach((ws: PartyWS) => {
+    // broken connection
+    if (ws.isAlive === false) {
+      ws.terminate()
+    }
+
+    ws.isAlive = false
+    ws.emit('ping')
+  })
+}, ms('30s'))
+
+partyServer.on('connection', (ws: PartyWS, req: HttpRequest) => {
+  // initialize connection
+  ws.isAlive = true
+  ws.user = req.user as User
+
+  /* Register events */
+
+  ws.on('ping', () => {
+    ws.emit('sendPartyMessage', 'ping')
+  })
+
+  ws.on('pong', () => {
+    ws.isAlive = true
+  })
+
+  /**
+   * Receive message and decode it.
+   * And then trigger proper event.
+   */
+  ws.on('message', msg => {
+    // decode received message
+    const message: PartyMessage = JSON.parse(msg as string)
+
+    // check if the operation is exist
+    if (ws.listeners(message.operation).length > 0) {
+      // trigger operation
+      ws.emit(message.operation, message.body)
+    } else {
+      // send error message
+      ws.emit('sendPartyMessage', 'error', 'invalid operation')
+    }
+  })
+
+  /**
+   * Send all party message in encoded format.
+   */
+  ws.on('sendPartyMessage', (operation, body?) => {
+    const message: PartyMessage = {
+      operation: operation
+    }
+
+    if (body !== null) {
+      message.body = body
+    }
+
+    ws.send(JSON.stringify(message))
+  })
+})
+
+partyServer.on('close', () => {
+  clearInterval(heartbeat)
+})
+
+export default partyServer
