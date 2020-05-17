fcd170ea7d7e1e50bba82efd8373ba9bd07a10a8
ihooni <to@ihooni.com>
2020-05-17 15:53:56 +0900

    chore: create party with restaurant model

--- src/modules/internal/party/PartyRoom.ts
+++ src/modules/internal/party/PartyRoom.ts
@@ -1,29 +1,37 @@
 import { PartyWS } from '@/modules/internal/party/partyServer'
 import { nanoid } from 'nanoid'
+import Restaurant from '@/models/Restaurant'
 
 export default class PartyRoom {
   public id: string
-  public restaurantID: number
+  public restaurant: Restaurant
   public title: string
   public address: string
   public capacity: number
   public members: PartyWS[]
 
-  public constructor(
+  public async createParty(
     restaurantID: number,
     title: string,
     address: string,
     capacity: number,
     hostWS: PartyWS
-  ) {
+  ): Promise<void> {
     ;[
       this.id,
-      this.restaurantID,
+      this.restaurant,
       this.title,
       this.address,
       this.capacity,
       this.members
-    ] = [nanoid(12), restaurantID, title, address, capacity, [hostWS]]
+    ] = [
+      nanoid(12),
+      await Restaurant.findByPk(restaurantID),
+      title,
+      address,
+      capacity,
+      [hostWS]
+    ]
 
     hostWS.roomID = this.id
   }
