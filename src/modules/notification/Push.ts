505ec172d31e86f03c56147a14372671ceab84f7
ihooni <to@ihooni.com>
2020-06-14 16:42:46 +0900

    chore: add module for push notification

---
+++ src/modules/notification/Push.ts
@@ -0,0 +1,32 @@
+import { Expo, ExpoPushMessage } from 'expo-server-sdk'
+import Logger from '@/modules/log/Logger'
+
+const expo = new Expo()
+
+export default class Push {
+  private validMessages: ExpoPushMessage[]
+
+  public addToMessageQueue(message: ExpoPushMessage): void {
+    if (Array.isArray(message.to)) {
+      message.to = message.to.filter(token => Expo.isExpoPushToken(token))
+    } else {
+      message.to = Expo.isExpoPushToken(message.to) ? [message.to] : []
+    }
+
+    if (message.to.length !== 0) {
+      this.validMessages.push(message)
+    }
+  }
+
+  public async sendPushMessages(): Promise<void> {
+    const chunks = expo.chunkPushNotifications(this.validMessages)
+
+    for (const chunk of chunks) {
+      try {
+        await expo.sendPushNotificationsAsync(chunk)
+      } catch (e) {
+        Logger.I.log('error', `SEND PUSH MESSAGES ERROR: ${e}`)
+      }
+    }
+  }
+}
